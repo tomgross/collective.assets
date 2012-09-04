ed38e3b6bf23b2cabd47454638afea27537e61c1
Tom Gross <itconsense@gmail.com>
2012-09-04 10:55:44 +0200

    fixed control panel adapter

--- src/collective/assets/interfaces.py
+++ src/collective/assets/interfaces.py
@@ -1,4 +1,7 @@
 from zope.interface import Interface
+from zope.schema import Bool
+
+from collective.assets import CollectiveAssetsMessageFactory as _
 
 class IAssetsConfig(Interface):
     """ """
@@ -6,3 +9,10 @@ class IAssetsConfig(Interface):
 
 class IAssetsSchema(Interface):
     """ """
+
+    active = Bool(title=_('assets_active_label', default=u'Active'),
+                  description=_('assets_active_help',
+                      default=u'Check this to enable the Assets integration'),
+                  default=False)
+
+
